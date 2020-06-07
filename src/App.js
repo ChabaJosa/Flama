1a11e6867ca2bc844baab11031f39cbd5aa21483
ChabaJosa <chabagjg@gmail.com>
2020-06-07 15:04:55 -0400

    Added structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React                      from 'react';
+import {Switch, Route }           from 'react-router-dom';
+import Navbar                     from "./components/navbar"
+import Details                    from "./components/details"
+import ProductList                from "./components/productList"
+import Cart                       from "./components/cart"
+import DefaultScreen              from "./components/defaultScreen"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css" 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar/>
+
+        <Switch>
+            <Route exact path="/"   component={ProductList}   ></Route>
+            <Route path="/details"  component={Details}       ></Route>
+            <Route path="/cart"     component={Cart}          ></Route>
+            <Route                  component={DefaultScreen} ></Route>
+        </Switch>
+    </>
   );
 }
 
---
+++ src/components/product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Product in ProductList</h3>
+            </div>
+        )
+    }
+}
