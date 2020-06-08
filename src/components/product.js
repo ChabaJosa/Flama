6061746171601008205918242d337e077237fc3e
ChabaJosa <chabagjg@gmail.com>
2020-06-07 21:30:24 -0400

    Added pictures and prices

--- src/components/product.js
+++ src/components/product.js
@@ -17,9 +17,34 @@ export default class Product extends Component {
                         <Link to="/details">
                             <img alt="product" src={img} className="card-img-top"  />
                         </Link>
+                        <button 
+                            className="cart-btn" 
+                            disabled= { inCart ? true : false } 
+                            onClick={() => {
+                                console.log("Added to cart")
+                            }}
+                        >
+                        { inCart ? (
+                            <p className="text-capitalize mb-0" disabled>
+                            {" "}
+                            in cart
+                            </p>
+                            ) : (
+                            <i className="fas fa-cart-plus" />
+                            )}
+                        </button>
+                    </div>
+                    {/* card footer */}
+                    <div className={'card-footer d-flex justify-content-between'} > 
+                            <p className={'align-self-center mb-0'}>
+                                {title}
+                            </p>
+                            <h5 className='text-blue font-italic mb-0' >
+                                <span className="mr-1" >$</span>
+                                {price} 
+                            </h5>
                     </div>
                 </div>
-                {/* <h3>Hello from Product in ProductList</h3> */}
             </ProductWrapper>
         )
     }
