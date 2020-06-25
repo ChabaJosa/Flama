e2b3b81dc5dfbde579c05442fce0f3c2dd9b2bf0
ChabaJosa <chabagjg@gmail.com>
2020-06-25 17:19:25 -0400

    Minor changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -43,10 +43,9 @@ export default class Navbar extends Component {
 
         <Link to="/cart" className="ml-auto">
           <ButtonContainer>
-            <span className="mr-2">
+            <span className="m-2">
               <i className="fas fa-cart-plus" />
             </span>
-            My Cart
           </ButtonContainer>
         </Link>
       </NavWrapper>
