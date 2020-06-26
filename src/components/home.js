270659cc7117730f4db60c0d32d8892e09bf29d2
ChabaJosa <chabagjg@gmail.com>
2020-06-26 19:16:27 -0400

    Working on the title css effects

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,13 @@ import ProductList from "./productList"
 
 export default function home() {
     return (
-        <>
+        <>  
+            <div className="minimalistFront" >
+                <h1 className="titleText titleAnimation" >Welcome</h1>
+                <h1 className="titleText titleAnimation" >to</h1>
+                <h1 className="titleText" id="FlamaTitle" > Flama</h1>
+
+            </div>
             <ProductList />
         </>
     )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -43,7 +43,7 @@ export default class Navbar extends Component {
 
         <Link to="/cart" className="ml-auto">
           <ButtonContainer>
-            <span className="m-2">
+            <span>
               <i className="fas fa-cart-plus" />
             </span>
           </ButtonContainer>
