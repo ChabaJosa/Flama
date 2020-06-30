efe67bb2ab8b0e84256aefbbdd8e0f664ef0d602
ChabaJosa <chabagjg@gmail.com>
2020-06-29 20:46:23 -0400

    Fixing title responsiveness

--- src/components/home.js
+++ src/components/home.js
@@ -5,9 +5,7 @@ export default function home() {
     return (
         <>  
             <div className="minimalistFront" >
-                <h1 className="titleText titleAnimation" >Welcome</h1>
-                <h1 className="titleText titleAnimation" >to</h1>
-                <h1 className="titleText" id="FlamaTitle" > Flama</h1>
+                <h1 className="titleText titleAnimation" >Welcome to <span id="FlamaTitle" > Flama</span> </h1> 
             </div>
             <ProductList />
         </>
