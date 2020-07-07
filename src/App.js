201d6176bc7c18411f8e50e1953499bf6ccced01
ChabaJosa <chabagjg@gmail.com>
2020-07-06 21:38:58 -0400

    Added meta tags and favicon

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
-import React                      from 'react';
-import {Switch, Route }           from 'react-router-dom';
-import Navbar                     from "./components/navbar"
-import Details                    from "./components/details"
-import Cart                       from "./components/cart"
-import DefaultScreen              from "./components/defaultScreen"
-import Modal                      from "./components/modal"
-import Home                       from "./components/home"
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import MetaTags from "react-meta-tags";
+
+import Navbar from "./components/navbar";
+import Details from "./components/details";
+import Cart from "./components/cart";
+import DefaultScreen from "./components/defaultScreen";
+import Modal from "./components/modal";
+import Home from "./components/home";
+
 // import BootstrapFooter            from "./components/bootstrapFooter"
 
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css" 
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-        <Navbar/>
+      <MetaTags>
+        <title>
+          Flama
+        </title>
+        <meta property="og:url" content="https://flamacandels.netlify.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Flama - Enlighten your life"
+        />
+        <meta
+          property="og:description"
+          content="Enlighten your life"
+        />
+        <meta property="og:image" content="../src/images/header.jpeg" />
 
-        <Switch>
-            <Route exact path="/"   component={Home}   ></Route>
-            <Route path="/details"  component={Details}       ></Route>
-            <Route path="/cart"     component={Cart}          ></Route>
-            <Route                  component={DefaultScreen} ></Route>
-        </Switch>
-        {/* <BootstrapFooter /> */}
-        <Modal />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:site"
+          content="https://flamacandels.netlify.com"
+        />
+        <meta name="twitter:creator" content="Carlos Hernandez" />
+        <meta
+          name="twitter:title"
+          content="Flama - Enlighten your life"
+        />
+        <meta
+          name="twitter:description"
+          content="Enlighten your life"
+        />
+        <meta name="twitter:image" content="../src/images/header.jpeg" />
+      </MetaTags>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={DefaultScreen}></Route>
+      </Switch>
+      {/* <BootstrapFooter /> */}
+      <Modal />
     </>
   );
 }
