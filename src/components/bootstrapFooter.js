7af721ead4f368d3b602aca645e226a82cc82c8a
ChabaJosa <chabagjg@gmail.com>
2020-07-14 14:00:35 -0400

    Resized background image and added conditionals for footer

--- src/components/bootstrapFooter.js
+++ src/components/bootstrapFooter.js
@@ -37,6 +37,8 @@ export default function BootstrapFooter() {
 
   const getPics = useCallback(() => {
     // Documentation: https://unsplash.com/oauth/applications/146767 process.env.REACT_APP_APP_ID
+
+    console.log("Here's the paypal key", process.env.REACT_APP_PAYPAL);
     const API_KEY = process.env.REACT_APP_UNSPLASH;
     const unsplash = `https://api.unsplash.com/photos/?client_id=${API_KEY}&page=1&q=candles`;
     axios.get(unsplash).then((result) => {
@@ -67,7 +69,7 @@ export default function BootstrapFooter() {
         <footer className="mt-1 b-footer">
           <div className="container">
             <div className="row">
-              <div className="col-lg-4 col-md-6" >
+              <div className="col-lg-4 col-md-6">
                 <h3>Site Map</h3>
                 <ul className="list-unstyled three-column">
                   <li>Home</li>
@@ -155,26 +157,43 @@ export default function BootstrapFooter() {
                   url={filteredResults[2].image.thumbnail.contentUrl}
                 />
               </div>
-              <div className="col-lg-4" >
-                  <h3>"Our Fav Pics of the day" </h3>
+
+              <div className="col-lg-4">
+                <h3>"Our Fav Pics of the day" </h3>
                 <img
-                  className="img-thumbnail b-footer-img" 
-                  src={ Images ? Images[0].urls.thumb : "http://placehold.it/150x100" }
+                  className="img-thumbnail b-footer-img"
+                  src={
+                    Images
+                      ? Images[0].urls.thumb
+                      : "http://placehold.it/150x100"
+                  }
                   alt=""
                 />
                 <img
-                  className="img-thumbnail b-footer-img" 
-                  src={ Images ? Images[0].urls.thumb : "http://placehold.it/150x100" }
+                  className="img-thumbnail b-footer-img"
+                  src={
+                    Images
+                      ? Images[1].urls.thumb
+                      : "http://placehold.it/150x100"
+                  }
                   alt=""
                 />
                 <img
-                  className="img-thumbnail b-footer-img" 
-                  src={ Images ? Images[0].urls.thumb : "http://placehold.it/150x100" }
+                  className="img-thumbnail b-footer-img"
+                  src={
+                    Images
+                      ? Images[2].urls.thumb
+                      : "http://placehold.it/150x100"
+                  }
                   alt=""
                 />
                 <img
-                  className="img-thumbnail b-footer-img" 
-                  src={ Images ? Images[0].urls.thumb : "http://placehold.it/150x100" }
+                  className="img-thumbnail b-footer-img"
+                  src={
+                    Images
+                      ? Images[3].urls.thumb
+                      : "http://placehold.it/150x100"
+                  }
                   alt=""
                 />
               </div>
@@ -187,6 +206,85 @@ export default function BootstrapFooter() {
       </>
     );
   } else {
-    return null;
+    return (
+      <>
+        <footer className="mt-1 b-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-6">
+                <h3>Site Map</h3>
+                <ul className="list-unstyled three-column">
+                  <li>Home</li>
+                  <li>Products</li>
+                  <li>About</li>
+                  {/* <li>Code</li>
+                <li>Design</li>
+                <li>Host</li>
+                <li>Contact</li>
+                <li>Company</li> */}
+                </ul>
+                <div className="row sm-div">
+                  <a
+                    href="Facebook.com"
+                    className="fa fa-facebook"
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      window.open("Facebook.com", "_blank");
+                    }}
+                  >
+                    <span></span>
+                  </a>
+                  <a
+                    href="Twitter.com"
+                    className="fa fa-twitter"
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      window.open("Twitter.com", "_blank");
+                    }}
+                  >
+                    <span></span>
+                  </a>
+                  <a
+                    href="Pinterest.com"
+                    className="fa fa-pinterest"
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      window.open("Pinterest.com", "_blank");
+                    }}
+                  >
+                    <span></span>
+                  </a>
+                  <a
+                    href="LinkedIn.com"
+                    className="fa fa-linkedin"
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      window.open("LinkedIn.com", "_blank");
+                    }}
+                  >
+                    <span></span>
+                  </a>
+                  <a
+                    href="Instagram.com"
+                    className="fa fa-instagram"
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      window.open("Instagram.com", "_blank");
+                    }}
+                  >
+                    <span></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </>
+    );
   }
 }
