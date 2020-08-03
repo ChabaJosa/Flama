9aa9832297dee5f92f8f0cc35eef9667a069cfb6
ChabaJosa <chabagjg@gmail.com>
2020-08-03 10:16:58 -0400

    Added some conditionals to bootstrapFooter to deal with API reqs

--- src/components/bootstrapFooter.js
+++ src/components/bootstrapFooter.js
@@ -47,6 +47,58 @@ export default function BootstrapFooter() {
     });
   }, []);
 
+  const ApiLessFooter = ({length}) => {
+    return (
+      <>
+        <div className={`col-lg-${length} col-md-6`} >
+          <h3>Follow us on Social Media</h3>
+          <div className="row sm-div">
+            <a
+              href="https://www.facebook.com/"
+              className="fa fa-facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span></span>
+            </a>
+            <a
+              href="https://www.Twitter.com/"
+              className="fa fa-twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span></span>
+            </a>
+            <a
+              href="https://www.Pinterest.com"
+              className="fa fa-pinterest"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span></span>
+            </a>
+            <a
+              href="https://www.LinkedIn.com"
+              className="fa fa-linkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span></span>
+            </a>
+            <a
+              href="https://www.Instagram.com"
+              className="fa fa-instagram"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span></span>
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   useEffect(() => {
     getNews();
     getPics();
@@ -55,207 +107,138 @@ export default function BootstrapFooter() {
   if (
     articleData !== null &&
     articleData.data !== undefined &&
+    articleData.data.value.length > 3 &&
     Images !== undefined
   ) {
     let filteredResults = articleData.data.value.filter((eachArticle) => {
       return eachArticle.image !== undefined;
     });
 
-    console.log("Here's Article Data", articleData);
-    console.log("Hey filtered results heere", filteredResults);
+    if (filteredResults.length > 3) {
+      console.log("Here's Article Data", articleData);
+      console.log("Hey filtered results heere", filteredResults);
 
-    return (
-      <>
-        <footer className="mt-1 b-footer">
-          <div className="container">
-            <div className="row">
-              
-              <div className="col-lg-4 col-md-6">
-                <h3>Latest Articles</h3>
-                <FooterArticle
-                  title={filteredResults[0].name}
-                  text={filteredResults[0].description}
-                  url={filteredResults[0].image.thumbnail.contentUrl}
-                />
-                <FooterArticle
-                  title={filteredResults[1].name}
-                  text={filteredResults[1].description}
-                  url={filteredResults[1].image.thumbnail.contentUrl}
-                />
-                <FooterArticle
-                  title={filteredResults[2].name}
-                  text={filteredResults[2].description}
-                  url={filteredResults[2].image.thumbnail.contentUrl}
-                />
-              </div>
+      return (
+        <>
+          <footer className="mt-1 b-footer">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-4 col-md-6">
+                  <h3>Latest Articles</h3>
+                  <FooterArticle
+                    title={filteredResults[0].name}
+                    text={filteredResults[0].description}
+                    url={filteredResults[0].image.thumbnail.contentUrl}
+                  />
+                  <FooterArticle
+                    title={filteredResults[1].name}
+                    text={filteredResults[1].description}
+                    url={filteredResults[1].image.thumbnail.contentUrl}
+                  />
+                  <FooterArticle
+                    title={filteredResults[2].name}
+                    text={filteredResults[2].description}
+                    url={filteredResults[2].image.thumbnail.contentUrl}
+                  />
+                </div>
 
-              <div className="col-lg-4">
-                <h3>"Our Fav Pics of the day" </h3>
-                <img
-                  className="img-thumbnail b-footer-img"
-                  src={
-                    Images
-                      ? Images[0].urls.thumb
-                      : "http://placehold.it/150x100"
-                  }
-                  alt=""
-                />
-                <img
-                  className="img-thumbnail b-footer-img"
-                  src={
-                    Images
-                      ? Images[1].urls.thumb
-                      : "http://placehold.it/150x100"
-                  }
-                  alt=""
-                />
-                <img
-                  className="img-thumbnail b-footer-img"
-                  src={
-                    Images
-                      ? Images[2].urls.thumb
-                      : "http://placehold.it/150x100"
-                  }
-                  alt=""
-                />
-                <img
-                  className="img-thumbnail b-footer-img"
-                  src={
-                    Images
-                      ? Images[3].urls.thumb
-                      : "http://placehold.it/150x100"
-                  }
-                  alt=""
-                />
+                <div className="col-lg-4">
+                  <h3>"Our Fav Pics of the day" </h3>
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[0].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[1].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[2].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[3].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
+                </div>
+                <ApiLessFooter length={4} />
+                {/* </div> */}
               </div>
-              <div className="col-lg-4 col-md-6">
-                <h3>Follow us on Social Media</h3>
-                {/* <ul className="list-unstyled three-column"> */}
-                  {/* <li>Home</li>
-                  <li>Products</li>
-                  <li>About</li> */}
-                  {/* <li>Code</li>
-                <li>Design</li>
-                <li>Host</li>
-                <li>Contact</li>
-                <li>Company</li> */}
-                {/* </ul> */}
-                <div className="row sm-div">
-                  <a
-                    href="https://www.facebook.com/"
-                    className="fa fa-facebook"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="https://www.Twitter.com/"
-                    className="fa fa-twitter"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="https://www.Pinterest.com"
-                    className="fa fa-pinterest"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="https://www.LinkedIn.com"
-                    className="fa fa-linkedin"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="https://www.Instagram.com"
-                    className="fa fa-instagram"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
+            </div>
+            <div className="copyright text-center">
+              Copyright © 2020 <span>Flama</span>
+            </div>
+          </footer>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <footer className="mt-1 b-footer">
+            <div className="container">
+              <div className="row">
+                <ApiLessFooter length={6} />
+
+                <div className="col-lg-6">
+                  <h3>"Our Fav Pics of the day" </h3>
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[0].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[1].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
+                  <img
+                    className="img-thumbnail b-footer-img"
+                    src={
+                      Images
+                        ? Images[2].urls.thumb
+                        : "http://placehold.it/150x100"
+                    }
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
-          </div>
-          <div className="copyright text-center">
-            Copyright © 2020 <span>Flama</span>
-          </div>
-        </footer>
-      </>
-    );
+            <div className="copyright text-center">
+              Copyright © 2020 <span>Flama</span>
+            </div>
+          </footer>
+        </>
+      );
+    }
   } else {
-    return (
-      <>
-        <footer className="mt-1 b-footer">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-4 col-md-6">
-                <h3>Site Map</h3>
-                <ul className="list-unstyled three-column">
-                  <li>Home</li>
-                  <li>Products</li>
-                  <li>About</li>
-                  {/* <li>Code</li>
-                <li>Design</li>
-                <li>Host</li>
-                <li>Contact</li>
-                <li>Company</li> */}
-                </ul>
-                <div className="row sm-div">
-                  <a
-                    href="Facebook.com"
-                    className="fa fa-facebook"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="Twitter.com"
-                    className="fa fa-twitter"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="Pinterest.com"
-                    className="fa fa-pinterest"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="LinkedIn.com"
-                    className="fa fa-linkedin"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                  <a
-                    href="Instagram.com"
-                    className="fa fa-instagram"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span></span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-      </>
-    );
+    return <ApiLessFooter length={12} />;
   }
 }
