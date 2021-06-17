be72ece008ce808121760bba8e5630714bdd243e
ChabaJosa <chabagjg@gmail.com>
2021-06-16 21:23:02 -0400

    Added media query

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -13,50 +13,48 @@ export default function LandingCarousel() {
   };
 
   return (
-    <>
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <div style={{ background: "rgba(0, 0, 0, 0.8)" }}>
-            <img
-              className="d-block w-100 carouselImage"
-              src={SomePic1}
-              alt="First slide"
-            />
-          </div>
-          <Carousel.Caption>
-            <h3>First thing</h3>
-            <p>flamma stands for this</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div style={{ background: "rgba(0, 0, 0, 0.8)" }}>
-            <img
-              className="d-block w-100 carouselImage"
-              src={SomePic2}
-              alt="Second slide"
-            />
-          </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <div style={{ background: "rgba(0, 0, 0, 0.8)" }}>
+          <img
+            className="d-block w-100 carouselImage"
+            src={SomePic1}
+            alt="First slide"
+          />
+        </div>
+        <Carousel.Caption>
+          <h3>First thing</h3>
+          <p>flamma stands for this</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{ background: "rgba(0, 0, 0, 0.8)" }}>
+          <img
+            className="d-block w-100 carouselImage"
+            src={SomePic2}
+            alt="Second slide"
+          />
+        </div>
 
-          <Carousel.Caption>
-            <h3>Second thing</h3>
-            <p>flamma stands for this</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div style={{ background: "rgba(0, 0, 0, 0.8)" }}>
-            <img
-              className="d-block w-100 carouselImage"
-              src={SomePic3}
-              alt="Third slide"
-            />
-          </div>
+        <Carousel.Caption>
+          <h3>Second thing</h3>
+          <p>flamma stands for this</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{ background: "rgba(0, 0, 0, 0.8)" }}>
+          <img
+            className="d-block w-100 carouselImage"
+            src={SomePic3}
+            alt="Third slide"
+          />
+        </div>
 
-          <Carousel.Caption>
-            <h3>Third thing</h3>
-            <p>flamma stands for this</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </>
+        <Carousel.Caption>
+          <h3>Third thing</h3>
+          <p>flamma stands for this</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
